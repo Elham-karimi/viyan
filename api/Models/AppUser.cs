ffb7e197f6b293e934e7dcff28d1117f59f06756
Elham Karimi <elhamka636@gmail.com>
2023-06-21 16:54:53 +0330

    first edit

---
+++ api/Models/AppUser.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Models;
+
+public record AppUser(
+    // [property: BsonId, BsonRepresentation(BsonType.ObjectId)] string? Id, //hamishe sabet
+    [MinLength(3), MaxLength(20)] string? Name,
+    [EmailAddress] string Email,
+    [MinLength(8)] string Password,
+    [Range(18, 99)] int Age,
+    bool IsAdmin
+);
